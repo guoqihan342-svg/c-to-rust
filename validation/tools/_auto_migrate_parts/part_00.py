2806305a6cae6fe3645014e1cc3a24eafeb882c8
guoqihan342 <guoqihan342@gmail.com>
2026-07-12 01:53:34 +0800

    refactor(auto-migrate): split oversized bootstrap fragment

--- validation/tools/_auto_migrate_parts/part_00.py
+++ validation/tools/_auto_migrate_parts/part_00.py
@@ -6,7 +6,9 @@ from sys import _getframe as _C2RSplitFrame
 _C2R_SPLIT_PARTS_DIR = _C2RSplitPath(_C2RSplitFrame().f_code.co_filename).with_name("part_00_split_parts")
 _C2R_SPLIT_PART_NAMES = (
     "part_00.pyfrag",
-    "part_01.pyfrag",
+    "part_01_00.pyfrag",
+    "part_01_01.pyfrag",
+    "part_01_02.pyfrag",
     "part_02.pyfrag",
     "part_03.pyfrag",
     "part_04.pyfrag",
